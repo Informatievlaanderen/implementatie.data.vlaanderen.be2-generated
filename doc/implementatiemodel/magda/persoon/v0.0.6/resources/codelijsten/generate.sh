d33e29144178efa11a29a6fb77933e89f8289e84
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2026-06-17 17:43:33 +0200

    Delete doc/implementatiemodel/magda directory

--- doc/implementatiemodel/magda/persoon/v0.0.6/resources/codelijsten/generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for file in *.csv; do
-    if [ -f "$file" ]; then
-        echo "$file"
-        npx @oslo-flanders/codelist-generator --input $file --output ${file%.csv}.ttl
-    fi
-done
